c0a1e2f91272eabb67ace7c7a47880fe4c48032a
dew613 <dewansunnah613@gmail.com>
2021-02-12 22:36:08 -0500

    added posters and the basic necessities

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -60,6 +61,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        
+        let posterURL = URL(string : baseURL + posterPath)
+        cell
+    
+        cell.posterView.af_setImage(withURL: posterURL!)
         
         return cell
     }
